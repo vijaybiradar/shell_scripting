d0311ace4fec86ee47d628a1b56605c12d4019ed
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2022-03-05 10:33:58 +0530

    Add files via upload

---
+++ Fundaments/io_redirect_1.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+cat <<EOF
+This is Intellipaats Shell Scripting Course. We are covering IO Redirections.
+Thanks.
+EOF
---
+++ Fundaments/testarg.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Understanding Arguments
+echo "I love to play" $1
+echo "I also like" $2
