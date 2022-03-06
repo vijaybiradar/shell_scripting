3be40d4ac45a2dcfd59b4c009402d1d9b95959da
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2022-03-06 09:47:15 +0530

    Update output_redirection_and_pipes.sh

--- Fundaments/output_redirection_and_pipes.sh
+++ Fundaments/output_redirection_and_pipes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Author : Vijaykumar S Biradar
 # Script follows here:
+cat file file1 file2 file3
+cat file file1 file2 file3 1>outfile 2>&1
 rm file42 file33 file1201 | grep file42 
 rm file42 file33 file1201 2>&1 | grep file42 
