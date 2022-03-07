32083dd6dfa55e8595f9c66058d6582c3faa6031
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2022-03-07 20:44:31 +0530

    Update Output_Redirection.sh

--- Fundaments/Output_Redirection.sh
+++ Fundaments/Output_Redirection.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Author : Vijaykumar S Biradar
 # Script follows here:
+#standard output eg
 echo hello everyone > listings
 cat listings
 echo thanks for reading1 >> listings
@@ -9,4 +10,19 @@ echo thanks for reading2 >> listings
 cat listings
 echo thanks for reading3 > listings
 cat listings
+#standard error eg
+cat listings123
+cat listings123 2>&1
+cat listings123
+#standard input eg
+cat <<EOF
+This is Shell Scripting Course. We are covering IO Redirections.
+Thanks.
+EOF
+cat <EOF
+This is Shell Scripting Course. We are covering IO Redirections.
+Thanks.
+EOF
+
+
 
