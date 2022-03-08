266488bd0aa09edee1e1ef58ae98bac937c3c3d7
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2022-03-08 13:43:28 +0530

    Update Output_Redirection.sh

--- Fundaments/Output_Redirection.sh
+++ Fundaments/Output_Redirection.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 # Author : Vijaykumar S Biradar
 # Script follows here:
-#standard output eg
-echo hello everyone > listings
-cat listings
-echo thanks for reading1 >> listings
-cat listings
-echo thanks for reading2 >> listings
-cat listings
-echo thanks for reading3 > listings
-cat listings
+#with and without standard output eg
+echo hello everyone > without_standardoutput.txt
+cat without_standardoutput.txt
+#redirects standard output to with_standardoutput.txt; This is also works same as above 
+echo hello everyone 1> with_standardoutput.txt
+cat with_standardoutput.txt
+echo thanks for reading1 >> without_standardoutput.txt
+cat without_standardoutput.txt
+echo thanks for reading2 >> without_standardoutput.txt
+cat without_standardoutput.txt
+echo thanks for reading3 > without_standardoutput.txt
+cat without_standardoutput.txt
+#redirects stdout to with_standardoutput.txt
+echo thanks for reading1 1>> with_standardoutput.txt
+cat listings.txt
+echo thanks for reading2 1>> with_standardoutput.txt
+cat listings.txt
+echo thanks for reading3 1> with_standardoutput.txt
+cat with_standardoutput.txt
+
 #standard error eg
-cat listings123
-cat listings123 2>&1
-cat listings123
+ls without_standardoutput.txt with_standardoutput.txt without_standarderror.txt
+cat without_standarderror.txt
+ls without_standardoutput.txt with_standardoutput.txt without_standarderror.txt 2> with_standarderror.txt
+cat with_standarderror.txt
+
 #standard input eg
-cat <<EOF
+cat > with_standardinput.txt <<EOF
 This is Shell Scripting Course. We are covering IO Redirections.
 Thanks.
 EOF
-cat <EOF
-This is Shell Scripting Course. We are covering IO Redirections.
-Thanks.
-EOF
+cat with_standardinput.txt 
+
 
 
 
